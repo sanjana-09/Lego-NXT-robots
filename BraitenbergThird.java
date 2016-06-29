2da71d152f8984e0ba601fcaade5e043376c21e1
Sanjana Khot <skhot1@sheffield.ac.uk>
2016-06-29 07:06:32 +0100

    minor formatting

--- BraitenbergThird.java
+++ BraitenbergThird.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class BraitenbergThird {
 	public static void main(String[] args) throws InterruptedException {
+
 		//Create a robot object to use and connect to it
-        Robot myRobot = new Robot();
+		Robot myRobot = new Robot();
 		int topCap = 700;
-        Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
-        Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
-        ColorSensor colorSensor = myRobot.getColorSensor(Sensor.Port.S1);
+		Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
+		Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
+		ColorSensor colorSensor = myRobot.getColorSensor(Sensor.Port.S1);
 		UltrasonicSensor leftSensor = myRobot.getUltrasonicSensor(Sensor.Port.S4);
 
-
-    	Scanner scan = new Scanner( System.in );
+		Scanner scan = new Scanner( System.in );
 		
 		//distance and light
 		double leftDistance = leftSensor.getDistance();
@@ -22,14 +22,13 @@ public class BraitenbergThird {
 		//normalise
 		double leftReading = 1.7 - Math.min(1.7,leftDistance);
 
-		
 		//r = f(intensity)
 		double intention = 1.6 - lightIntensity;
 
 		//motivation
 		double motivation;
 		System.out.print("Enter a number from 0 to 1:");
-    	motivation = scan.nextFloat();
+		motivation = scan.nextFloat();
 		System.out.println("Motivation:" + motivation);
 	
 		//e= r-i
@@ -58,43 +57,39 @@ public class BraitenbergThird {
 			leftDistance = leftSensor.getDistance();
 			lightIntensity = colorSensor.getAmbient();
 
-			
 			//normalise both
 			leftReading = 1.7 - Math.min(1.7,leftDistance);
-			
 			intention = 1.6 - lightIntensity;
 			
 			//e = r-i
 			error = intention - leftReading;
 			
-		if (error > 0) {
-			leftV = (int)Math.round(baseSpeed + 1000*motivation*Math.abs(error));
-			leftV = Math.min(leftV, topCap);
-		}
-		
-		//right speed
-		if (error < 0) {
-			rightV = (int)Math.round(baseSpeed + 1000*motivation*Math.abs(error));
-			rightV = Math.min(rightV, topCap);
-		}
-		
-			//set speeds
-			leftMotor.setSpeed(leftV);
-			rightMotor.setSpeed(rightV);
+			if (error > 0) {
+				leftV = (int)Math.round(baseSpeed + 1000*motivation*Math.abs(error));
+				leftV = Math.min(leftV, topCap);
+			}
 			
-			//left move
-			leftMotor.forward();
-			System.out.println("left unhappy");
-			System.out.println("leftspeed: " + leftV);
+			//right speed
+			if (error < 0) {
+				rightV = (int)Math.round(baseSpeed + 1000*motivation*Math.abs(error));
+				rightV = Math.min(rightV, topCap);
+			}
 
-	
-			//right move
-			rightMotor.forward();
-			System.out.println("right unhappy");
-			System.out.println("rightspeed: " + rightV);
+		//set speeds
+		leftMotor.setSpeed(leftV);
+		rightMotor.setSpeed(rightV);
 
-			
-			Thread.sleep(500);
+		//left move
+		leftMotor.forward();
+		System.out.println("left unhappy");
+		System.out.println("leftspeed: " + leftV);
+
+		//right move
+		rightMotor.forward();
+		System.out.println("right unhappy");
+		System.out.println("rightspeed: " + rightV);
+
+		Thread.sleep(500);
 		}
 		
 	}
